c1bb4d782841e89929968c9201a4cb2bc998dda4
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-03 19:34:30 +0100

    Initial commit

---
+++ src/Day1.java
@@ -0,0 +1,15 @@
+public class Day1 {
+
+
+    public static void main(String[] args){
+
+
+      Test t
+
+      t = null;
+
+
+
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+
+    Test t = new Test();
+    System.out.println("t");
+
+    t = null;
+
+    Home h = new Home();
+
+}
