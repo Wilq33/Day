5d47b760fcdc52134c91729406adc6445b1a3adc
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-06 22:58:09 +0100

    randomNumber static double and working on WhileLoop

--- src/Random.java
+++ src/Random.java
@@ -1,24 +1,24 @@
 public class Random {
 
+    public static double randomNumber = Math.round(Math.random() * 10);
+
     public static void main(String[] args){
 
-
         //Random number between 0 and 10
-        double random = Math.round(Math.random() * 10);
-        System.out.println("The random number is: "+ random);
+       System.out.println("The random number is: "+ randomNumber);
 
-        if(random == 5){
+        if(randomNumber == 5){
 
-            System.out.println(random + " is the lucky number");
-        }else if(random >= 6 && random <= 9){
+            System.out.println(randomNumber + " is the lucky number");
+        }else if(randomNumber >= 6 && randomNumber <= 9){
 
-            System.out.println(random + " is greater than 5 but less than 10");
-        }else if(random ==10){
+            System.out.println(randomNumber + " is greater than 5 but less than 10");
+        }else if(randomNumber == 10){
 
-            System.out.println(random + " is the winning number");
+            System.out.println(randomNumber + " is the winning number");
         }else{
 
-            System.out.println(random + " is less than 5");
+            System.out.println(randomNumber + " is less than 5");
         }
 
     }
---
+++ src/WhileLoop.java
@@ -0,0 +1,18 @@
+public class WhileLoop {
+
+
+
+
+    public static void main(String[] args){
+
+
+        while(Random.randomNumber <= 5){
+
+            System.out.println(Random.randomNumber);
+        }
+
+
+    }
+
+
+}
