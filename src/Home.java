a2a6f5d2a38509c7cd1e70f9b5a165814bdb87cd
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-06 19:10:59 +0100

    Adding house1 and house2 objects

--- src/Home.java
+++ src/Home.java
@@ -3,27 +3,45 @@ public class Home {
 
     int i;
     //display
+    int j;
 
-    public void Display(){
+
+    //void
+    public void display(){
+
+
 
         System.out.println("Text inside display Method");
     }
 
-
-    public String Word(){
+    //string
+    public String word(){
 
         String x = "Display";
 
         return "Word";
     }
 
-    public int number(){
+
+    //number
+    public int number(int k, String a, char c, float f){
+
+        k = 20;
+        f = 12.5f;
+        a ="Display";
 
         return 5;
     }
 
 
     public static void main(String[] args){
+
+        Home house1 = new Home();
+        house1.display();
+
+        Home house2 = new Home();
+        house2.word();
+        //house.number(k, a, c, f);
 
 
     }
