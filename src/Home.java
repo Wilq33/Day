ed0433eff2ea937a59f1576f52adde285d1f9cf6
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-06 18:55:12 +0100

    Adding word and number method

--- src/Home.java
+++ src/Home.java
@@ -4,20 +4,26 @@ public class Home {
     int i;
     //display
 
-    public void display(){
+    public void Display(){
 
-        String x = "Display";
-
-
+        System.out.println("Text inside display Method");
     }
 
 
+    public String Word(){
+
+        String x = "Display";
+
+        return "Word";
+    }
+
+    public int number(){
+
+        return 5;
+    }
 
 
     public static void main(String[] args){
-
-
-
 
 
     }
