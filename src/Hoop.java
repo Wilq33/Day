f6d52316e7405f4b623a38c2d14f11264732a2ac
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-11 21:56:44 +0100

    Commented out for loop, added nested loops

--- src/Hoop.java
+++ src/Hoop.java
@@ -1,15 +1,33 @@
 public class Hoop {
 
     public static void main(String[] args){
-        
-        double i = Random.randomNumber;
 
-        for(;i<=20;){
+       /* double m = Random.randomNumber;
 
-            System.out.print(i);
+        for(;m<=20;){
+
+            System.out.print(m);
+            m++;
+        }*/
+       
+        int i=0;
+
+        while(i<3){
+
+            for(int j=0; j<4; j++)
+            {
+                int k = 0;
+                do {
+
+                    System.out.println("i=" + i  +" j="+ j +" k=" + k);
+                    k++;
+
+                } while (k < 5);
+
+            }
             i++;
-        }
 
+        }
 
     }
 
