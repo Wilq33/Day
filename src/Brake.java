555efabd895a5a0026f81fb245b377ab96237304
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-11 22:19:42 +0100

    Added loop with break

---
+++ src/Brake.java
@@ -0,0 +1,30 @@
+public class Brake {
+
+
+    public void breakAndContinue(){
+
+        System.out.println("First");
+
+        //break;
+
+        //continue;
+
+        System.out.println("Last");
+
+    }
+
+    public static void main(String[] args){
+
+        /*Brake b = new Brake();
+        b.breakAndContinue();*/
+
+        for(int i=0; i<10; i++){
+
+
+            if(i==5)
+            break;
+            
+            System.out.println(i);
+        }
+    }
+}
