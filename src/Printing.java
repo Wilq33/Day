7a8a966909983ea46934fae3c9bb91d4f01c4277
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-12 20:23:41 +0100

    Simple methods

---
+++ src/Printing.java
@@ -0,0 +1,30 @@
+public class Printing {
+
+    public static void print(){
+
+        System.out.println("Checking");
+
+
+    }
+
+
+    public static int a = 30;
+    public static int b = 20;
+
+    public static void addNumbers(){
+
+        int c = a + b;
+        System.out.println("a + b = " + c);
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        print();
+        addNumbers();
+
+
+    }
+}
