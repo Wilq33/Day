595497a41b4f034cf3995f0ead5d86a05e435dfb
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-19 22:00:29 +0100

    Overloading exercises

---
+++ src/Sample/Overloading.java
@@ -0,0 +1,16 @@
+package Sample;
+
+public class Overloading {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void add(int a, int b){
+
+    }
+
+    public void add(int a, int b, int c){
+
+    }
+}
--- src/Sample/SampleClass.java
+++ src/Sample/SampleClass.java
@@ -2,6 +2,17 @@ package Sample;
 
 public class SampleClass {
 
+    public void getHomeLoanROI (String bankName, int amount){
+
+
+    }
+
+    public void getHomeLoanROI (int amount, String bankName){
+
+
+    }
+
+
     public static void main(String[] args) {
 
 
