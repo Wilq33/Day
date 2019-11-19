70b345c4f583d9f0940fb4a72cc4eaafbbf55d17
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-19 22:29:00 +0100

    Overloading exercises 2

--- src/Sample/Overloading.java
+++ src/Sample/Overloading.java
@@ -4,13 +4,34 @@ public class Overloading {
 
     public static void main(String[] args) {
 
+        Overloading obj = new Overloading();
+        obj.login(123456789, "password");
+        obj.add(10, 20, 30);
+        byte b1 = 25;
+        byte b2 = 45;
+        obj.add(b1, b2);
+
     }
 
-    public void add(int a, int b){
+    public void add(byte a, byte b){
+
+        System.out.println(a + b);
 
     }
 
     public void add(int a, int b, int c){
+
+        System.out.println(a + b + c);
+
+    }
+
+    public void login (String username, String password){
+
+    }
+
+    public void login (int mobile, String password){
+
+        System.out.println("Login successful");
 
     }
 }
